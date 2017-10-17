7e2068f147cac2be31356a2bdfabadd4683da13a
esther <estheralvaro44@gmail.com>
2017-10-17 11:25:16 -0500

    login and redirect

--- src/login.js
+++ src/login.js
@@ -3,10 +3,10 @@ import Utils from './Utils.js';
 import './login.css';
 import {
     BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
+    Route,
+    Switch,
+    NavLink,
+    Redirect
 } from 'react-router-dom'
 
 class Login extends Component {
@@ -45,54 +45,35 @@ class Login extends Component {
                     <div className="row">
                         <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                             <i className="fa fa-question-circle" id="ayuda" aria-hidden="true"></i>
-                            <img src="./img/bb6.gif" className="img-responsive bebe-logo" alt="" />
+                            <img src="https://s-media-cache-ak0.pinimg.com/originals/56/46/4d/56464d61a65d71268b245c52485280c7.gif" className="img-responsive bebe-logo" alt="" />
                             <div className="img-transparent">
                                 <h1 className="letra-1">Mis Chispitas</h1>
                             </div>
                             <hr className="hr" />
                             <h3 className="letra-2">"Mi niño saludable"</h3>
                             <h3 className="iniciar">Iniciar Sesión</h3>
-                            <form>
-                                <div className="form-group">
-                                    <input type="number" className="form-control input-1" id="input-1" name="dni" placeholder="D.N.I" onChange={(e) => this.inputDni(e)} required />
-                                </div>
-                                <div className="col-md-12 checket">
-                                    <div className=" col-md-6 col-xs-6">
-                                      <div className="form-check valida">
-                                        <label className="form-check-label">
-                                            <input type="checkbox" onChange={(e) => this.checkMom(e)} />Mamá
-                                        </label>
-                                       </div>
-                                      </div>
-                                      <div className="col-sm-6 col-md-6 col-xs-6">
-                                        <div className="form-check valida">
-                                          <label className="form-check-label">
-                                             <input type="checkbox" onChange={(e) => this.checkNurse(e)} />Enfermera</label>
-                                          </label>
+                            <div className="row ">
+                                <div className="col-lg-12">
+                                    <div className="input-group buscador">
+                                        <div className="input-group-btn">
+                                            <button  type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Action <span className="caret" /></button>
+                                            <ul className="dropdown-menu">
+                                                <li><a href="#">Madre</a></li>
+                                                <li role="separator" className="divider" />
+                                                <li><a href="#">Enfermera</a></li>
+                                            </ul>
                                         </div>
-                                      </div>
-                                  </div>
-                            {
-                                   this.state.activeMom ? 
-                                    <NavLink to={'/welcome'} type="submit" className="btn btn-default shake" id="button">ENTRAR</NavLink>
-                                    :
-                                    <NavLink to={'/welcome'} type="submit" className="btn btn-default shake" id="button" disabled>ENTRAR</NavLink> 
+                                        <input  type="text" className="form-control" aria-label="..." />
+                                    </div>
+                                    <NavLink to={'/welcome'} type="submit" className="btn btn-default shake" id="button">Iniciar Sesión</NavLink>
+                                </div>
+                            </div>
 
-                                }
-                                 {
-                                   this.state.activeNurse? 
-                                    <NavLink to={'/home'} type="submit" className="btn btn-default shake" id="button">ENTRAR</NavLink>
-                                    :
-                                    <NavLink to={'/home'} type="submit" className="btn btn-default shake" id="button" disabled>ENTRAR</NavLink> 
-
-                                }
-                        </form>
-
+                            </div>
                         </div>
                     </div>
-                </div>
             </center>
-        )
+                )
     }
 }
 export default Login;
